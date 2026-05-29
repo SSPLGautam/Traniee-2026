8595fe06bda89664968d318a22419cf844b4c8c0
Priya Sharma <sspl.priya2026@gmail.com>
2026-05-29 14:47:20 +0530

    cokkie auth project

---
+++ Priya Sharma/Cookie Auth Project/RoleBasedAuth/Controllers/UserController.cs
@@ -0,0 +1,136 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using RoleBasedAuth.Models.Domain;
+using RoleBasedAuth.Models.DTO;
+using System.Linq.Expressions;
+
+namespace RoleBasedAuth.Controllers
+{
+    public class UserController : Controller
+    {
+        private readonly UserManager<ApplicationUser> _userManager;
+        private readonly SignInManager<ApplicationUser> _signInManager;
+
+        public UserController( UserManager<ApplicationUser> userManager, SignInManager<ApplicationUser> signInManager)
+        {
+            _userManager = userManager;
+            _signInManager = signInManager;
+        }
+
+        [HttpGet]
+        public IActionResult Register()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Register(Register model)
+        {
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    var user = new ApplicationUser
+                    {
+                        Name = model.Name,
+                        Email = model.Email,
+                        UserName = model.Email,
+                        Status = true
+                    };
+
+                    var result = await _userManager.CreateAsync(user, model.Password);
+
+                    if (result.Succeeded)
+                    {
+                        await _userManager.AddToRoleAsync(user, "User");
+
+                        return RedirectToAction("Login");
+                    }
+
+                    foreach (var error in result.Errors)
+                    {
+                        ModelState.AddModelError("", error.Description);
+                    }
+                }
+                catch (Exception ex)
+                {
+                    ModelState.AddModelError("", "Something went wrong. Please try again.");
+
+                }
+            }
+                return View(model);
+        }
+
+        [HttpGet]
+        public IActionResult Login()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Login(Login model)
+        {
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    var user = await _userManager.FindByEmailAsync(model.Email);
+
+                    if (user == null)
+                    {
+                        ModelState.AddModelError("", "Invalid Email or Password");
+                        return View(model);
+                    }
+
+
+                    if (user.Status == false)
+                    {
+                        ModelState.AddModelError("", "Oops! You are inactive....");
+                        return View(model);
+                    }
+
+                    var result =
+                        await _signInManager.PasswordSignInAsync(
+                            user.UserName,
+                            model.Password,
+                            model.RememberMe,
+                            false);
+
+                    if (result.Succeeded)
+                    {
+                        return RedirectToAction("Index", "Home");
+                    }
+
+                    ModelState.AddModelError("", "Invalid Email or Password");
+
+                }
+                catch (Exception)
+                {
+                    ModelState.AddModelError("", "Something went wrong. Please try again.");
+
+                }
+            }
+            return View(model);
+        }
+        
+
+        public async Task<IActionResult> Logout()
+        {
+            try
+            {
+                await _signInManager.SignOutAsync();
+            }
+            catch(Exception)
+            {
+                return RedirectToAction("Login");
+
+            }
+            return RedirectToAction("Login");
+        }
+
+        public IActionResult AccessDenied()
+        {
+            return View();
+        }
+    }
+}
